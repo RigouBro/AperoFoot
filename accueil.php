93ffefdbacfc56807abb7075fcbf15d9737a3eed
RigouBro <48955834+RigouBro@users.noreply.github.com>
2019-04-04 09:57:25 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,28 @@
+<?php
+include("mise_en_page.php");
+
+entete("Accueil");
+
+menu_nav();
+?>
+<div class="banner_accueil">
+	<div class="image_accueil"></div>
+	<div class="title_accueil">
+		<h1> Avec qui allez vous regarder le match ce soir ?</h1>
+		<a href="accueil_recherche.php" class="bouton_recherche">Rechercher un match</a>
+	</div>
+</div>
+<div class="article_accueil">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vulputate metus nec nisi consequat hendrerit. Quisque velit diam, imperdiet ut libero ac, pulvinar posuere odio. Fusce velit ligula, aliquam eget suscipit sit amet, interdum et orci. Ut quis rhoncus diam, ut feugiat enim. Sed at ipsum ut neque luctus pharetra in id lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque sem nulla, porttitor ut nibh in, commodo venenatis tellus. Aenean maximus eros in mi dapibus mollis. Sed mattis placerat interdum. In pulvinar tincidunt urna, quis condimentum velit. Morbi ligula est, sollicitudin vel sapien non, gravida blandit dui. Curabitur dignissim ullamcorper lectus nec maximus. Proin aliquet id dui et volutpat. Fusce molestie metus augue, at sagittis leo aliquam convallis. Morbi pretium est nec dolor feugiat, at rutrum neque faucibus. Curabitur at orci eu ante pharetra ornare vel at nisl.
+
+Morbi non dignissim quam. Vestibulum vel nisl ut est feugiat interdum sed vel turpis. Quisque pharetra vehicula magna in ultricies. Ut non molestie nisl, quis efficitur mauris. Nullam eu mattis sem. Nullam rutrum scelerisque congue. Sed diam mi, finibus id mollis sit amet, interdum sit amet magna.
+
+Maecenas at odio est. Aenean consequat magna vitae augue viverra imperdiet. Ut iaculis mi at nunc laoreet, eget interdum massa semper. Vestibulum sed urna at neque pharetra vehicula vel non sem. Sed nec erat malesuada, pellentesque ex et, posuere turpis. Nulla facilisi. Praesent elit elit, mollis eu ante ut, interdum dignissim orci. Nunc eleifend convallis dui, eu hendrerit urna cursus non. Aliquam sed leo sed urna porta molestie. Sed efficitur turpis vitae magna rhoncus, in facilisis sapien auctor. Ut quis nibh libero. In sodales nisi purus, eu semper odio ultrices posuere. Ut vel est non ex pretium placerat viverra quis sem. Donec euismod dolor pharetra leo hendrerit dapibus. Nulla facilisi.
+
+Morbi dictum facilisis dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse potenti. Aliquam malesuada, ipsum ac posuere viverra, tortor orci lacinia quam, nec aliquam est dui tempus ligula. Morbi interdum blandit orci a vehicula. Duis tempor hendrerit ipsum, non tristique ipsum mollis sed. Mauris blandit, ex vitae gravida hendrerit, sapien arcu euismod dolor, a fermentum lorem augue quis libero. Suspendisse ultricies neque rhoncus lectus aliquam viverra. Quisque vitae eros lectus. Aliquam ac porta mi, id eleifend arcu. In hac habitasse platea dictumst. Mauris euismod, massa at rutrum tempor, velit diam volutpat lectus, efficitur mollis sem orci at lacus. Curabitur ac auctor tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+</div>
+
+<?php
+pied();
+
+?>
