38d953fd8ad4ca9ee51054e954bc6ebe43ef53ae
RigouBro <48955834+RigouBro@users.noreply.github.com>
2019-05-13 18:39:09 +0200

    Add files via upload

---
+++ partial/menu_moncompte.php
@@ -0,0 +1,43 @@
+<?php
+function informations($id){
+    ?>
+
+
+    <div class="membre_formulaire">
+
+        <span class="membre_info">Nom :</span>
+
+        <span class="info">Ici $nom de l'$id</span>
+
+    </div>
+
+    <div class="membre_formulaire">
+
+        <span class="membre_info">Prénom :</span>
+
+        <span class="info">Ici $prenom de l'$id</span>
+
+    </div>
+
+    <div class="membre_formulaire">
+
+        <span class="membre_info">Adresse de messagerie :</span>
+
+        <span class="info">Ici $mail de l'$id</span>
+
+    </div>
+
+    <div class="membre_formulaire">
+
+        <span class="membre_info">Mot de Passe :</span>
+
+        <span class="info">Ici $password de l'$id</span>
+
+    </div>
+
+
+
+<?php
+}
+?>
+
