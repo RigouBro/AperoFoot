7ae7f9852a666a13fba799eb83add45ddbc11181
Theo RIGAL <theo.rigal@pc253-56.pedago.ensiie.fr>
2019-05-14 11:40:57 +0200

    a

--- traitement_ajout.php
+++ traitement_ajout.php
@@ -22,12 +22,17 @@ $email=$_POST['adresse_mail'];
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=aperofoot;charset=utf8', 'root', '');
+    
 }
 catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
 
+
+$requete = $bdd->exec('SELECT * FROM user');
+
+print $requete;;
 
 $req = $bdd->prepare('INSERT INTO user(password, nom, prenom, email) VALUES(:password, :nom, :prenom, :email');
 $req->execute(array(
